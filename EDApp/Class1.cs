27e1cfc736e6787c520127e7e5a7869a9aff8078
omkarsabale099 <omkarsabale099@gmail.com>
2023-01-17 15:07:53 +0530

    Encyption/Decryption

---
+++ EDApp/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDApp
+{
+    internal class Class1
+    {
+        public static void Encrypt()
+        {
+            EncryptionDecryptionData.EncryptFile(@"D:\WorkSpace\Data\Output.txt", @"D:\WorkSpace\Data\Input.txt", @"D:\WorkSpace\PGPKeys\publickey.asc", true, true);
+
+        }
+
+        public static void Decrypt() 
+        {
+            EncryptionDecryptionData.DecryptFile(@"D:\WorkSpace\Data\Output.txt", @"D:\WorkSpace\PGPKeys\privatekey.asc", "Pass@123".ToCharArray(), @"D:\WorkSpace\Data\DecryptedData.txt");
+
+        }
+    }
+}
---
+++ EDApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using EDApp;
+using System;
+using Org.BouncyCastle.Bcpg.OpenPgp;
+using Org.BouncyCastle.Utilities.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Encryption Press 1");
+        Console.WriteLine("Decryption Press 2");
+
+        Console.WriteLine("Enter Your Choice:");
+
+        int iChoice = Convert.ToInt32(Console.ReadLine());
+
+
+        switch (iChoice)
+        {
+            case 1:
+                Class1.Encrypt();
+
+                break;
+
+            case 2:
+
+                Class1.Decrypt();
+
+                break;
+
+            default:
+                Console.WriteLine("Enter Valid Input");
+                Console.WriteLine("Encryption Press 1");
+                Console.WriteLine("Decryption Press 2");
+                Console.WriteLine("Enter Your Choice:");
+                int iChoice1 = Convert.ToInt32(Console.ReadLine());
+                if (iChoice == 1)
+                    goto case 1;
+                else if (iChoice1 == 2)
+                    goto case 2;
+                else
+                    goto default;
+    
+        }
+
+    }
+}
